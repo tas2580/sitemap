3bd5b605925fd46c79981007c7ee5b543dbfeab7
tas2580 <tobi.schaefer@gmail.com>
2015-11-11 18:54:47 +0100

    first release

---
+++ README.md
@@ -0,0 +1,27 @@
+<img src="https://tas2580.net/downloads/image-11.png" width="600" height="80" alt="phpBB 3.1 - SEO URLs" />
+
+This is an extension for the phpBB forums software. You need an instalation of phpBB 3.1.x to use this.
+
+DESCRIPTION
+-------
+Changes the URLs of forums and topics too more SEO friendly URLs with the title of the forums and topics in it. This
+extension is held as simple as possible, it contains no ACP, you can just install it and use the SEO friendly URLs.
+
+INSTALATION
+----------
+To install this extension download it from here and upload the files in your forum under <b>/ext/tas2580/sitemap</b>.
+After that go to the Admin panel of your forum and navigate in to Customise -> Extension Management -> Extensions. Search this extension in the list of extensions and click on Enable.
+
+After the installation you can find your sitemap under YOUR_FORUM_URL/sitemap.xml
+
+SUPPORT
+-------
+You can get support for this extension on <a href="https://www.phpbb.com/community/viewtopic.php?f=456&t=0">phpbb.com</a>
+or in german on <a href="https://www.phpbb.de/community/viewtopic.php?f=149&t=0">phpbb.de</a>. For more informations look at
+<a href="https://tas2580.net/downloads/phpbb-sitemap/">my Website</a>.
+
+LICENSE
+-------
+<a href="http://opensource.org/licenses/gpl-2.0.php">GNU General Public License v2</a>
+
+[![Build Status](https://travis-ci.org/tas2580/sitemap.svg?branch=master)](https://travis-ci.org/sitemap)
---
+++ controller/sitemap.php
@@ -0,0 +1,95 @@
+<?php
+/**
+*
+* @package phpBB Extension - tas2580 Social Media Buttons
+* @copyright (c) 2014 tas2580 (https://tas2580.net)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace tas2580\sitemap\controller;
+
+class sitemap
+{
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+	/** @var \phpbb\db\driver\driver */
+	protected $db;
+	/** @var \phpbb\controller\helper */
+	protected $helper;
+	/** @var \phpbb\template\template */
+	protected $template;
+	/** @var string php_ext */
+	protected $php_ext;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\auth\auth			$auth		Auth object
+	* @param \phpbb\controller\helper	$helper
+	* @param \phpbb\template\template	$template
+	*/
+	public function __construct(\phpbb\auth\auth $auth, \phpbb\db\driver\driver_interface $db, \phpbb\controller\helper $helper, \phpbb\template\template $template, $php_ext)
+	{
+		$this->auth = $auth;
+		$this->db = $db;
+		$this->helper = $helper;
+		$this->template = $template;
+		$this->php_ext = $php_ext;
+	}
+
+	public function sitemap($id)
+	{
+		$board_url = generate_board_url();
+		$sql = 'SELECT forum_name, forum_last_post_time
+			FROM ' . FORUMS_TABLE . '
+			WHERE forum_id = ' . (int) $id;
+		$result = $this->db->sql_query($sql);
+		$row = $this->db->sql_fetchrow($result);
+
+		$this->template->assign_block_vars('urlset', array(
+			'URL'			=> $board_url . 'viewforum.' . $this->php_ext . '?f=' . $id,
+			'TIME'		=> gmdate('Y-m-d\TH:i:s+00:00', (int) $row['forum_last_post_time']),
+		));
+		$sql = 'SELECT topic_id, topic_title, topic_last_post_time, topic_status
+			FROM ' . TOPICS_TABLE . '
+			WHERE forum_id = ' . (int) $id;
+		$result = $this->db->sql_query($sql);
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			if ($row['topic_status'] <> ITEM_MOVED)
+			{
+				$this->template->assign_block_vars('urlset', array(
+					'URL'			=> $board_url .  'viewtopic.' . $this->php_ext  . '?f=' . $id . '&t='. $row['topic_id'],
+					'TIME'		=> ($row['topic_last_post_time'] <> 0)  ? gmdate('Y-m-d\TH:i:s+00:00', (int) $row['topic_last_post_time']) : '',
+				));
+			}
+		}
+
+		return $this->helper->render('sitemap.html');
+	}
+
+	public function index()
+	{
+		header('Content-Type: application/xml');
+
+		$board_url = generate_board_url();
+		$sql = 'SELECT forum_id, forum_name, forum_last_post_time
+			FROM ' . FORUMS_TABLE . '
+			WHERE forum_type = ' . (int) FORUM_POST . '
+			ORDER BY left_id ASC';
+		$result = $this->db->sql_query($sql);
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			if ($this->auth->acl_get('f_list', $row['forum_id']))
+			{
+				$this->template->assign_block_vars('forumlist', array(
+					'URL'			=> $this->helper->route('tas2580_seourls_sitemap', array('id' => $row['forum_id']), true, '', \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL),
+					'TIME'		=>($row['forum_last_post_time'] <> 0) ? gmdate('Y-m-d\TH:i:s+00:00', (int) $row['forum_last_post_time']) : '',
+				));
+			}
+		}
+
+		return $this->helper->render('sitemap_index.html');
+	}
+}
