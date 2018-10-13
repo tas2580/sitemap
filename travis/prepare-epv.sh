6bde0d09bda88089200265ef4c60794bc97a35b0
tas2580 <tobi.schaefer@gmail.com>
2018-10-13 19:56:21 +0200

    travis fix

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -1,8 +1,8 @@
+
 #!/bin/bash
 #
-# PayPal Donation extension for the phpBB Forum Software package.
+# extension for the phpBB Forum Software package.
 #
-# @copyright (c) 2016 Matt Friedman
 # @license GNU General Public License, version 2 (GPL-2.0)
 #
 set -e
@@ -14,6 +14,7 @@ TRAVIS_PHP_VERSION=$2
 if [ "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.5" ]
 then
 	cd phpBB
-	composer require phpbb/epv:dev-master --dev --no-interaction
+	composer remove sami/sami --update-with-dependencies --dev --no-interaction
+	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
 	cd ../
 fi
