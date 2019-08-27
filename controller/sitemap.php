1fe1414863e1d871d875130bbd5a4617e29f28ee
v12mike <v12mike@ingenious.co.nz>
2019-08-27 01:54:52 -0400

    Remove the redundant f= parameter from viewtopic links so that the url provided by the sitemap matches the canonical url of the forum pages.

--- controller/sitemap.php
+++ controller/sitemap.php
@@ -115,7 +115,7 @@ class sitemap
 			do
 			{
 				// URL for topic
-				$url = $this->board_url . '/viewtopic.' . $this->php_ext . '?f=' . $id . '&amp;t=' . $topic_row['topic_id'];
+				$url = $this->board_url . '/viewtopic.' . $this->php_ext . '?t=' . $topic_row['topic_id'];
 				if ($start > 0)
 				{
 					$url .= '&amp;start=' . $start;
